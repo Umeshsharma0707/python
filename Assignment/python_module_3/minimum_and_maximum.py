80e882edf227a30f102376545cad00a9ff5c0f85
umesh sharma <109520296+Umeshsharma0707@users.noreply.github.com>
2024-01-01 08:53:27 +0000

    python programs

---
+++ Assignment/python_module_3/minimum_and_maximum.py
@@ -0,0 +1,11 @@
+#Write a Python program to find the maximum and minimum
+#numbersfrom the specified decimal numbers.
+
+decimal_nums = [10.1,25.5,36.9,25.1]
+
+min_deci = min(decimal_nums) # for min
+max_deci = max(decimal_nums) # for max
+
+print("maximum :", max_deci)
+print("minimum :", min_deci)
+
